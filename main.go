527b0aa3655a2c221836440dc6965e9cd520f4b4
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2023-04-05 20:48:14 +0900

    基本機能完成

--- main.go
+++ main.go
@@ -2,31 +2,72 @@ package main
 
 import (
 	"net/http"
-	"github.com/gin-gonic/gin"
+	"strconv"
 	"todo/mypkg"
+
+	"github.com/gin-gonic/gin"
 )
 
-    func main() {
-		// デフォルトのミドルウェアで新しい gin ルーターを作成する
-		// logger とアプリケーションクラッシュをキャッチする recovery ミドルウェア
-		router := gin.Default()
-		router.LoadHTMLGlob("templates/*.html")
-		mypkg.DbInit()
+func main() {
+	// デフォルトのミドルウェアで新しい gin ルーターを作成する
+	// logger とアプリケーションクラッシュをキャッチする recovery ミドルウェア
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+	mypkg.DbInit()
 
-		//index
-		router.GET("/", func(c *gin.Context){
-			todos := mypkg.DbGetAll()
-			c.HTML(http.StatusOK, "index.html",gin.H{
-				"todos": todos,
-			})
+	//index
+	router.GET("/", func(cxt *gin.Context) {
+		todos := mypkg.DbGetAll()
+		cxt.HTML(http.StatusOK, "index.html", gin.H{
+			"todos": todos,
 		})
+	})
 
-		//Create
-		router.POST("/new", func(c *gin.Context){
-			text := c.PostForm("text")
-			status := c.PostForm("status")
-			mypkg.DbInsert(text, status)
-			c.Redirect(302, "/")
+	//Create
+	router.POST("/new", func(cxt *gin.Context) {
+		text := cxt.PostForm("text")
+		status := cxt.PostForm("status")
+		mypkg.DbInsert(text, status)
+		cxt.Redirect(302, "/")
+	})
+
+	//Detail
+	router.GET("/detail/:id", func(cxt *gin.Context) {
+		n := cxt.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		todo := mypkg.DbGetOne(id)
+		cxt.HTML(http.StatusOK, "detail.html", gin.H{
+			"todo": todo,
 		})
-		router.Run()
-	}
+	})
+
+	//Update
+	router.POST("/update/:id", func(cxt *gin.Context) {
+		n := cxt.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		text := cxt.PostForm("text")
+		status := cxt.PostForm("status")
+		mypkg.DbUpdate(id, text, status)
+		cxt.Redirect(302, "/")
+
+	})
+
+	//Delete
+	router.GET("/delete/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		mypkg.DbDelete(id)
+		ctx.Redirect(302, "/")
+	})
+
+	router.Run()
+}
--- mypkg/config.go
+++ mypkg/config.go
@@ -2,33 +2,35 @@ package mypkg
 
 import (
 	"log"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-const(
-	DBNAME = "study"
-	USERNAME = "root"
-	PASSWORD = ""
-	IPADDRESS = "127.0.0.1"
+const (
+	DBNAME     = "study"
+	USERNAME   = "root"
+	PASSWORD   = ""
+	IPADDRESS  = "127.0.0.1"
 	PORTNUMBER = "3306"
 )
 
-func DatabaseConnection()(*gorm.DB, error){
+func DatabaseConnection() (*gorm.DB, error) {
 	dsn := USERNAME + ":" + PASSWORD + "@tcp(" + IPADDRESS + ":" + PORTNUMBER + ")/" + DBNAME + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	return db,err
+	return db, err
 }
-//DB初期化
-func DbInit(){
+
+// DB初期化
+func DbInit() {
 	db, err := DatabaseConnection()
 	if err != nil {
 		log.Fatal(err)
 	}
 	db.AutoMigrate(&Todo{})
-    }
-	
-//DB追加
+}
+
+// DB追加
 func DbInsert(text string, status string) {
 	db, err := DatabaseConnection()
 	if err != nil {
@@ -37,7 +39,7 @@ func DbInsert(text string, status string) {
 	db.Create(&Todo{Text: text, Status: status})
 }
 
-//DB全取得
+// DB全取得
 func DbGetAll() []Todo {
 	db, err := DatabaseConnection()
 	if err != nil {
@@ -46,4 +48,39 @@ func DbGetAll() []Todo {
 	var todos []Todo
 	db.Order("created_at desc").Find(&todos)
 	return todos
+}
+
+// DB1件取得
+func DbGetOne(id int) Todo {
+	db, err := DatabaseConnection()
+	if err != nil {
+		panic("データベースが開けません")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	return todo
+}
+
+// DB更新
+func DbUpdate(id int, text string, status string) {
+	db, err := DatabaseConnection()
+	if err != nil {
+		panic("データベースが開けません")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	todo.Text = text
+	todo.Status = status
+	db.Save(&todo)
+}
+
+// DB削除
+func DbDelete(id int) {
+	db, err := DatabaseConnection()
+	if err != nil {
+		panic("データベースが開けません")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	db.Delete(&todo)
 }
